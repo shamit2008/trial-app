f4c4a3df01f6694296d4197062822b483e807bd4
Shaurya Mittal <shauryamittal5@gmail.com>
2020-11-13 10:42:31 -0800

    class 11/13

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  const [website, setWebsite] = useState('www.google.com');
+  const [websiteSafety, setSafety] = useState(0); // scale from 0-100 or something
+
+  const displayInfo = () => {
+    // use website state to read csv file and get the info
+    // set the websiteSafety state with that value
+    // setSafety()
+  }
+
+  const handleSubmit = (event) => {
+    console.log(event.target.value);
+    setWebsite(event.target.value); // 1.
+    // displayInfo(website)
+    event.preventDefault();
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{website}</p>
+
+        <form onSubmit={handleSubmit}>
+          <label>
+            Name:
+          </label>
+          <input />
+          <input type="submit" value="Submit" />  {/* look into using forms/inputs with useState, google inputs and useState and there should tutorials */}
+        </form>
+        {
+        /* 1. have this form, 2. when the form submits, call a handleSubmit function to update the state, 3. in that function, call a function for displaying webite info */}
+        {/* 
+        <button onClick = {() => setWebsite('www.bing.com')}>Click Me!</button>
+        <button onClick = {() => setWebsite('www.yahoo.com')}>No, click me!</button> */}
+
+        <p>{website}'s safety score: {websiteSafety}</p> {/* style this is css */}
       </header>
     </div>
   );
 }
 
 export default App;
+
+// React Router - have more than one page to your app, tutorials online
+// Turn your excel file into CSV, readd that file into this project
+// Styling, functionality
