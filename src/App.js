cb05b30d86487989f2523aba6189b14f783e8df2
Shaurya Mittal <shauryamittal5@gmail.com>
2020-11-17 21:00:28 -0800

    hw 2

--- src/App.js
+++ src/App.js
@@ -2,49 +2,63 @@ import logo from './logo.svg';
 import './App.css';
 import React, {useState} from 'react';
 
+
 function App() {
   const [website, setWebsite] = useState('www.google.com');
   const [websiteSafety, setSafety] = useState(0); // scale from 0-100 or something
-
+  const [resultstyle, setstyle] = useState("color: 'red', fontSize: 40, textAlign:'center'" );
   const displayInfo = () => {
     // use website state to read csv file and get the info
     // set the websiteSafety state with that value
     // setSafety()
   }
 
-  const handleSubmit = (event) => {
-    console.log(event.target.value);
-    setWebsite(event.target.value); // 1.
-    // displayInfo(website)
-    event.preventDefault();
+  const handleSubmit = (evt) => {
+    evt.preventDefault(); // prevent function misfires
+    alert(`Calculating Score for ${website}`)
+    displayInfo(); // calling the function to calculate
+    resultStyle = {
+      color: 'blue', fontSize: 40, textAlign:"center",visibility: "visible"
+    }
   }
 
+  let resultStyle = {
+    color: 'red', fontSize: 40, textAlign:"center" 
+  }
   return (
+
     <div className="App">
       <header className="App-header">
-        <p>{website}</p>
+        <h3>Website security</h3>
 
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit}> {/* when you click submit, this will call handleSubmit */}
           <label>
-            Name:
+            Website Name: 
+            <input
+              type="text"
+              value={website}
+              onChange={e => setWebsite(e.target.value)}
+            />
           </label>
-          <input />
-          <input type="submit" value="Submit" />  {/* look into using forms/inputs with useState, google inputs and useState and there should tutorials */}
+          <input type="submit" value="Submit" />
         </form>
-        {
-        /* 1. have this form, 2. when the form submits, call a handleSubmit function to update the state, 3. in that function, call a function for displaying webite info */}
-        {/* 
-        <button onClick = {() => setWebsite('www.bing.com')}>Click Me!</button>
-        <button onClick = {() => setWebsite('www.yahoo.com')}>No, click me!</button> */}
 
-        <p>{website}'s safety score: {websiteSafety}</p> {/* style this is css */}
+        <p style={resultStyle}>{website}'s safety score: {websiteSafety}</p> {/* style this is css */}
       </header>
     </div>
   );
 }
 
-export default App;
 
+export default App;
+// hw:
+  // 1. look up tutorials on reading csv files with javascript + react - probably need useEffect hook
+  // 2. look up some stuff with CSS to make the first page look better
+ 
+  // next week: csv file, react router 
+
+
+// Later:
 // React Router - have more than one page to your app, tutorials online
 // Turn your excel file into CSV, readd that file into this project
 // Styling, functionality
--- src/index.js
+++ src/index.js
@@ -4,13 +4,22 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+/*
+handleFiles = files => {
+  var reader = new FileReader();
+  reader.onload = function(e) {
+      // Use reader.result
+      alert(reader.result)
+  }
+  reader.readAsText(files[0]);
+} */
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
-);
-
+  document.getElementById('root'),
+ 
+)
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
