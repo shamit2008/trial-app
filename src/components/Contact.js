54c48af196dc57e9a0dda710a8b755b13ea27a1a
Shaurya Mittal <shauryamittal5@gmail.com>
2021-01-09 11:18:31 -0800

    pages

---
+++ src/components/Contact.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+
+function Contact() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a>
+          
+        </a>
+        <h3>Contact Us</h3>
+        
+        <p>To report a problem with this app or its classifications, please contact us at <a src = "mailto:shauryamittal5@gmail.com">shauryamittal5@gmail.com</a></p>
+      </header>
+    </div>
+  );
+}
+ 
+
+export default Contact;
+
